2c28006c8df37c145e34c725071d3275c257a379
Varun Singh <varun7singh10@gmail.com>
2022-03-21 18:17:17 +0530

    2nd

--- musik.js
+++ musik.js
@@ -12,28 +12,68 @@
     const a5 = document.getElementById('a5')
     const a6 = document.getElementById('a6')
 
-    box1.addEventListener("click", e => {
-        console.log("a")
-        a1.play()
-    })
-    box2.addEventListener("click", e => {
-        console.log("a")
-        a2.play()
-    })
-    box3.addEventListener("click", e => {
-        console.log("a")
-        a3.play()
-    })
-    box4.addEventListener("click", e => {
-        console.log("a")
-        a4.play()
-    })
-    box5.addEventListener("click", e => {
-        console.log("a")
-        a5.play()
-    })
-    box6.addEventListener("click", e => {
-        console.log("a")
-        a6.play()
-    })
+    function control(e)
+    {
+        if(e.keyCode === 82)
+        {
+            //R
+            box4.classList.add("box4p")
+            a4.play()
+            setTimeout(function ()  {
+                box4.classList.remove("box4p")
+            },100)
+        }
+        if(e.keyCode === 84)
+        {
+            //T
+            box5.classList.add('box5p')
+            a5.play()
+            setTimeout(function ()  {
+                box5.classList.remove("box5p")
+            },100)
+
+        }
+        if(e.keyCode === 89)
+        {
+            //Y
+            box6.classList.add('box6p')
+            a6.play()
+            setTimeout(function ()  {
+                box6.classList.remove("box6p")
+            },50)
+
+        }
+        if(e.keyCode === 81)
+        {
+            //Q
+            box1.classList.add('box1p')
+            a1.play()
+            setTimeout(function ()  {
+                box1.classList.remove("box1p")
+            },100)
+
+        }
+        if(e.keyCode === 87)
+        {
+            //W
+            box2.classList.add('box2p')
+            a2.play()
+            setTimeout(function ()  {
+                box2.classList.remove("box2p")
+            },100)
+
+        }
+        if(e.keyCode === 69)
+        {
+            //E
+            box3.classList.add('box3p')
+            a3.play()
+            setTimeout(function ()  {
+                box3.classList.remove("box3p")
+            },100)
+
+        }
+    }
+
+    document.addEventListener("keydown", control)
 
