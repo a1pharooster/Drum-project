1d621e3b41989d5985ff41fbb9afc1e51e732692
Varun Singh <varun7singh10@gmail.com>
2022-03-21 17:32:59 +0530

    1st

---
+++ musik.js
@@ -0,0 +1,39 @@
+
+    const box1 = document.querySelector(".box1");
+    const box2 = document.querySelector(".box2");
+    const box3 = document.querySelector(".box3");
+    const box4 = document.querySelector(".box4");
+    const box5 = document.querySelector(".box5");
+    const box6 = document.querySelector(".box6");
+    const a1 = document.getElementById('a1')
+    const a2 = document.getElementById('a2')
+    const a3 = document.getElementById('a3')
+    const a4 = document.getElementById('a4')
+    const a5 = document.getElementById('a5')
+    const a6 = document.getElementById('a6')
+
+    box1.addEventListener("click", e => {
+        console.log("a")
+        a1.play()
+    })
+    box2.addEventListener("click", e => {
+        console.log("a")
+        a2.play()
+    })
+    box3.addEventListener("click", e => {
+        console.log("a")
+        a3.play()
+    })
+    box4.addEventListener("click", e => {
+        console.log("a")
+        a4.play()
+    })
+    box5.addEventListener("click", e => {
+        console.log("a")
+        a5.play()
+    })
+    box6.addEventListener("click", e => {
+        console.log("a")
+        a6.play()
+    })
+
